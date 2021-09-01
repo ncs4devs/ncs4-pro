661721748e05db0562036a6640f7212bd1e982da
Andrew Hood <andrew.hood@usm.edu>
2021-09-01 16:41:29 -0500

    Added Membership link, Refactored and worked on mobile navbar.

--- header.php
+++ header.php
@@ -47,22 +47,39 @@
 			</div><!-- .header-col.header-col1 -->
 			<div class="site-header-col site-header-col__col2">
 				<div class="col-inner">
+					<!-- Container for membership link & small mobile buttons -->
+					<div class="header-widget-area__small">
+						<!-- Membership Button -->
+						<div id="header-membership">
+							<!-- Connect Link -->
+							<a href="https://connect.ncs4.usm.edu/">Membership</a>
+						</div><!-- #header-membership-->
+						<div id="mobile_search-bar-toggle" class="mobile-widget">
+							<button class="search-bar-toggle" aria-controls="header-search-bar" aria-expanded="false">
+								<span class="screen-reader-text">Open search bar</span>
+							</button><!-- .search-bar-toggle -->
+						</div><!-- #mobile_search-bar-toggle -->
+						<div id="mobile_navbar-toggle" class="mobile-widget">
+							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+								<span class="dashicons dashicons-menu"/>
+							</button>
+						</div><!-- #mobile_navbar-toggle-->
+					</div><!-- .header-widget-area__small -->
 					<!-- Search bar widget -->
 					<div id="header-search-widget" class="header-widget-area widget-area">
 						<div id="header-search-widget__inner" class="header-widget-area__inner widget-area__inner">
-							<form class="search-form" method="get" action="/" role="search" itemprop="potentialAction" itemtype="https://schema.org/SearchAction">
+							<form id="header-search-bar" class="search-form" method="get" action="/" role="search" itemprop="potentialAction" itemtype="https://schema.org/SearchAction">
 								<label class="search-form-lbl screen-reader-text" for="searchform">Search</label>
 								<input id="searchform" class="search-form-input" type="search" name="s" placeholder="Search" itemprop="query-input"><button class="search-form-submit" type="submit" value="Search" title="Search">
 									<span class="screen-reader-text">Submit</span>
 								</button>
 								<meta content="/?s={s}" itemprop="target">
 							</form>
-						</div>
-					</div>
+						</div><!-- #header-search-widget__inner -->
+					</div><!-- #header-search-widget -->
 					<!-- Navbar -->
 					<div id="header-navbar-area" class="header-widget-area">
 						<nav id="header-navbar" class="main-navigation">
-							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'ncs4-pro' ); ?></button>
 							<?php
 							include get_stylesheet_directory() . '/ncs4-nav-walker.php';
 							wp_nav_menu(
