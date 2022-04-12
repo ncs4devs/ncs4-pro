fed17cdc9d26ba1b45244407c210b71ac74998f2
Andrew Hood <w10029145@usm.edu>
2022-04-12 10:22:27 -0500

    Added item attrs to nav walker

--- ncs4-nav-walker.php
+++ ncs4-nav-walker.php
@@ -1,6 +1,6 @@
 <?php
 class NCS4_Nav_Walker extends Walker_Nav_Menu {
-	function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
+	function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
 
 		/* Note: it's commonly recommended to use <span> for all links where
 		$item -> url == '#' (i.e. a menu item which isn't a page).
@@ -9,13 +9,45 @@ class NCS4_Nav_Walker extends Walker_Nav_Menu {
 		many accessibility programs. Just leave it as it is.)
 		*/
 
+    /*
 		$output .= '<li class="' . implode(" ", (array) $item -> classes) . '">';
 		$output .= '<a href="' . $item -> url . '">';
 		$output .= $item -> title;
 		$output .= '</a>';
+    */
 
-		$output .= $args -> walker -> has_children
+    $atts           = array();
+		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+		$atts['target'] = ! empty( $item->target ) ? $item->target : '';
+		if ( '_blank' === $item->target && empty( $item->xfn ) ) {
+			$atts['rel'] = 'noopener';
+		} else {
+			$atts['rel'] = $item->xfn;
+		}
+		$atts['href']         = ! empty( $item->url ) ? $item->url : '';
+		$atts['aria-current'] = $item->current ? 'page' : '';
+
+    $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( is_scalar( $value ) && '' !== $value && false !== $value ) {
+				$value       = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+
+    $item_output  = $args->before;
+		$item_output .= '<a' . $attributes . '>';
+		$item_output .= $args->link_before . $item -> title . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		$item_output .= $args -> walker -> has_children
 			? '<button class="submenu-toggle"></button>'
 			: '';
+
+    $output .= '<li class="' . implode(" ", (array) $item -> classes) . '">';
+		$output .= $item_output;
 	}
 }
