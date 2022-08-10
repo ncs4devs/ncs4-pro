9787c2f347b0cad0b066c02c9782a2869507f541
Andrew Hood <w10029145@usm.edu>
2022-08-10 14:19:09 -0500

    Changed default excerpt_more text

--- template-parts/content.php
+++ template-parts/content.php
@@ -58,7 +58,7 @@
   			sprintf(
   				wp_kses(
   					/* translators: %s: Name of current post. Only visible to screen readers */
-  					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'ncs4-pro' ),
+  					__( 'Read more<span class="screen-reader-text"> "%s"</span>', 'ncs4-pro' ),
   					array(
   						'span' => array(
   							'class' => array(),
