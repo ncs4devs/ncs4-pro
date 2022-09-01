c0ff5bb447d8eccd17dbe7d6749dece7251d031e
Andrew Hood <w10029145@usm.edu>
2022-09-01 12:16:43 -0500

    Basis 404 template

--- 404.php
+++ 404.php
@@ -14,43 +14,12 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ncs4-pro' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Error 404: Page not found', 'ncs4-pro' ); ?></h1>
 			</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ncs4-pro' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ncs4-pro' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$ncs4_pro_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ncs4-pro' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ncs4_pro_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+			<div class="page-content" style="margin-bottom: 5rem;">
+        <p>The page &ldquo;<?= $_SERVER['REQUEST_URI'] ?>&rdquo; could not be found.</p>
+				<p><a href="/">Return home</a></p>
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
