7854b389c914097593329e8c87b004638e6b7f1b
Aayush Gautam <w10153693@usm.edu>
2022-10-26 13:06:13 -0500

    Managed-the-Redirects

--- page-intercollegiate.php
+++
@@ -1,4 +0,0 @@
-<?php
-$url = "https://web.cvent.com/event/fc7ffae2-92c7-40fb-930d-fd4c06758829/summary";
-
-header('refresh: 0; url=/redirect?url=' . $url);
--- page-marathon.php
+++
@@ -1,4 +0,0 @@
-<?php
-$url = "https://web.cvent.com/event/d4dce194-59e3-44a5-9eba-658206337aac/summary";
-
-header('refresh: 0; url=/redirect?url=' . $url);
