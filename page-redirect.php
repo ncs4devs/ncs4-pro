c2229c1e9889d8624bc8345bf4a9a1e27866e013
Aayush Gautam <53272422+L65FREAD@users.noreply.github.com>
2024-09-26 11:10:57 +0100

    redirection changes

--- page-redirect.php
+++ page-redirect.php
@@ -1,34 +1,40 @@
 <?php
   $url = htmlspecialchars($_GET["url"]);
   $isValid = filter_var($url, FILTER_VALIDATE_URL);
-  if ($isValid) {
+  $validDomains = ['ncs4.usm.edu', 'usm.edu', 'cvent.me', 'web.cvent.com'];
+
+  function getDomain($url) {
+    $host = parse_url($url, PHP_URL_HOST); // Get the host part of the URL
+    return $host ? $host : false;
+  }
+
+  $urlDomain = getDomain($url);
+  $isDomainValid = $urlDomain && in_array($urlDomain, $validDomains);
+
+  if ($isValid && $isDomainValid) {
     header("refresh: 1; url=" . $url);
   }
 ?>
+
 <?php get_header(); ?>
-  <main
-    id="primary"
-    class="site-main site-main__redirect"
-  >
+  <main id="primary" class="site-main site-main__redirect">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
       <div class="entry-content">
         <div
           class="ncs4-site-margin ncs4-site-margin__size-small wp-block-ncs4-custom-blocks-margin"
-          style="
-            padding-top: 3rem;
-            padding-bottom: 3rem;
-            margin-top: 3rem;
-            margin-bottom: 3rem;
-          "
-        >
+          style="padding-top: 3rem; padding-bottom: 3rem; margin-top: 3rem; margin-bottom: 3rem;">
+
           <?php
-            if ($isValid) {
+            if ($isValid && $isDomainValid) {
               echo "<p>Please wait while you are redirected. If the new page does not load, click <a href='"
                 . $url . "' target='_blank'>here</a>.</p>";
-            } else {
-              echo "<p>Something went wrong. Please try again later. (<code class='error'>Err: Invalid url</code>)</p>";
+            } elseif (!$isValid) {
+              echo "<p>Something went wrong. Please try again later. (<code class='error'>Err: Invalid URL format</code>)</p>";
+            } elseif (!$isDomainValid) {
+              echo "<p>The URL provided is not from a valid domain. Please use a valid domain (<code class='error'>Err: Invalid domain</code>).</p>";
             }
           ?>
+
         </div><!-- .ncs4-site-margin -->
       </div><!-- .entry-content -->
     </article><!-- #post-<?php the_ID(); ?> -->
